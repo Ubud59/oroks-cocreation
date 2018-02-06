a959c54a018656115ebea2f07bd649904c677ca3
chloe bataille <chloe.bataille@decathlon.com>
2018-02-06 15:40:22 +0100

    stores

---
+++ src/store/tests/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  id: null,
+  type: null,
+  testReference: null,
+  title: null,
+  product: null,
+  status: null,
+  description: null,
+  validationTreshold: null,
+  timing: null,
+  imgSrc: null,
+  evaluationFormPath: null,
+  evaluationResultsPath: null,
+};
