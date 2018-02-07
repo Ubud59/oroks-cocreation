f68928ac9586a2f4af5861c49ebce6bf7df4a93e
Damien LEPRETRE <damien.lepretre@decathlon.com>
2018-02-07 12:21:25 +0100

    add server logic for authentication

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 const PG = require("pg");
 const path = require("path");
-const app = express();
+const authServices = require("./services/auth.services")
+const fetch = require("node-fetch");
 
+const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,6 +29,33 @@ app.get("/api/tests",
       });
     }
 );
+
+app.get("/api/auth", function (request, result) {
+  const authorizeUri = authServices.getAuthorizeUri();
+
+  result.redirect(authorizeUri);
+});
+
+// CALLBACK URL FOR DKT CONNECT TO SEND CODE
+app.get("/auth/callback", function (request, result) {
+  const tokenRequestParams = authServices.getTokenFromCode(request.query.code);
+
+  fetch(tokenRequestParams.uri, {
+    method: "POST",
+    body:    tokenRequestParams.params,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  })
+    .then(res => res.json())
+    .then(token => result.redirect(authServices.getFrontRedirectUri(token)))
+    .catch(error => console.warn(error));
+});
+
+app.get("/api/me", function(request, result) {
+  const token = request.headers.authorization
+  authServices.fetchUser(fetch, token)
+    .then(user => result.json(user))
+    .catch(e => result.status(500).send(e));
+})
 
 app.use("/static", express.static(path.join(__dirname, "../build/static")));
 
