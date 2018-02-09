305c093419fd8890ceda98c45341c0065c55a06f
Virginie Zinck <virginie.zinck@decathlon.com>
2018-02-09 13:57:27 +0100

    updated bug

--- src/modules/home/App.js
+++ src/modules/home/App.js
@@ -5,7 +5,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LoginComponent from '../login/Login';
 import MyTestsComponent from '../myTests/MyTests';
 import AuthComponent from '../auth/Auth'
-import TestNewComponent from '../testNew/TestNew';
 import ProfileComponent from '../profile/Profile';
 import TestComponent from '../test/Test';
 
