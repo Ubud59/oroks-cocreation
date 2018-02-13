c15dc6543972fed49338cf32d9898b68662bbe58
chloe bataille <chloe.bataille@decathlon.com>
2018-02-13 17:23:59 +0100

    wip

---
+++ src/modules/AllTests/Alltests.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+
+import { getTestsState } from '../../store/tests/selectors';
+import { updateTests } from '../../store/tests/actions';
+import { fetchAllTests } from '../../utils/alltests.services.js';
+
+import translateLabel from '../../utils/translateLabel.js';
+
+
+class AllTests extends Component {
+
+  componentDidMount(){
+
+    fetchAllTests()
+    .then(tests => {
+      console.log("je suis sans component did mount",tests);
+      return tests;
+    })
+    .then(tests => this.props.fetchAllTests(tests))
+    .catch(error => console.warn(error));
+  }
+
+  render() {
+
+    return (
+      <div className="row">
+           <div>{this.props.tests.map((test, index) =>
+
+        <div key={index} className="col-sm-6 col-md-3">
+        <div className="card-deck">
+        <div className="card">
+                <img className="card-img-top" src={test.image_src} alt="Card image cap"></img>
+                <div className="card-block">
+                  <h4 className="card-title">{test.title}</h4>
+                  <p className="card-text">{test.description}</p>
+                  <p>type de test : {translateLabel(test.type)}</p>
+                  <p>produit à tester : {test.product}</p>
+                  <p>{test.validationTreshold}</p>
+                  <p>numero de reference : {test.test_reference}</p>
+                  <p>statut : {test.status}</p>
+                </div>
+          <div className="card-footer">
+            <small className="text-muted">{test.timing}
+                <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                <div className="btn-group mr-2" role="group" aria-label="First group">
+                <div className="btn-group mr-2" role="group" aria-label="Second group">
+                <a className="btn btn-secondary btn-sm" href={`/test/${test.id}`} role="button">Modifier le test.</a>
+                </div>
+                </div>
+                </div>
+            </small>
+          </div>
+        </div>
+        </div>
+        </div>
+              )}
+            </div>
+          )
+            }
+      </div>
+      );
+      }
+      }
+
+
+const AllTestsComponent = connect(getTestsState, updateTests)(AllTests)
+
+export default AllTestsComponent;
