bf8862de0d3be68b0ee88c475a9a7cdb287d4f84
Damien LEPRETRE <damien.lepretre@decathlon.com>
2018-02-14 10:29:23 +0100

    fix bad directory name

--- src/modules/home/App.js
+++ src/modules/home/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import LoginComponent from '../login/Login';
 import MyTestsComponent from '../myTests/MyTests';
-import AllTestsComponent from '../allTests/AllTests';
+import AllTestsComponent from '../AllTests/AllTests';
 import AuthComponent from '../auth/Auth'
 import HomeComponent from './Home';
 import ProfileComponent from '../profile/Profile';
