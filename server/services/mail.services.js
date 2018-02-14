914afda8897110703ac9a623b0371e107e9923f7
Virginie Zinck <virginie.zinck@decathlon.com>
2018-02-14 14:06:46 +0100

    petites modifs mails

--- server/services/mail.services.js
+++ server/services/mail.services.js
@@ -14,14 +14,14 @@ const generateMailOptions = (userProfile, test) =>  {
           <FONT face="Verdana">
           <h1> Salut à toi testeur de la communauté Oroks ! </h1>
           <p >
-            ${userProfile.first_name}, Dans le cadre du développement de notre produit : ${test.product}, nous sommes à la recherche de testeurs en vue de réaliser un test et nous avons pensé à toi !
+            <strong>${userProfile.first_name}</strong>, Dans le cadre du développement de notre produit : <strong>${test.product}</strong>, nous sommes à la recherche de testeurs et nous avons pensé à toi !
             Alors si tu as envie de participer à ce test, n’attend plus et valide ton invitation !
           </p>
           <div style="padding-left: 25%; padding-right: 25%; justify-content: center;">
             <div style="border: 1px solid black; background-color: #F0F0F0; margin-top: 5%; padding-bottom: 10%;">
               <img style="width:100%;height:auto;" src=${test.image_src} alt="">
               <h1>${test.title}</h1>
-              <p> Viens tester le patin FIT 100 en FIITTING pour nous aider à améliorer notre produit. </p>
+              <p> Viens tester notre produit : <strong>${test.product}</strong> pour nous aider à l'améliorer. </p>
               <div style="padding-left: 5%; padding-bottom: 5%;">
                 <h4 style="text-align: left;"> Type de test :</h4>
                 <p style="text-align: left;"> ${translateLabel.translateLabel(test.type)} </p>
